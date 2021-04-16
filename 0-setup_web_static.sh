becda8285e1c751e65823006bfdbe05dd5f040e1
Andres Lopez <profesor.de.ingles@aol.com>
2021-04-16 01:31:33 +0000

    UPDATED | removed extra exit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,17 +6,16 @@ apt-get -y install nginx
 sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me https://github.com/andylopezr permanent;" /etc/nginx/sites-available/default
 sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default
 sed -i "/redirect_me/ a\\\terror_page 404 /404.html;" /etc/nginx/sites-available/default
-service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<head>
-        </head>
-        <body>
-            Holberton School
-        </body>
-    </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
