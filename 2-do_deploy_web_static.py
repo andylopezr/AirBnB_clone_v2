9996f9e671bac26ecdcccbd137387ac0416594ab
Andres Lopez <profesor.de.ingles@aol.com>
2021-04-16 02:25:08 +0000

    ADDED | Fab script to distribute an archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fab script to distribute an archive to web servers"""
+
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['35.190.169.147', '34.75.12.144']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
